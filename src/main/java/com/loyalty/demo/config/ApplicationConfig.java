36bda0d1f20c990119d57ed26e11458bf4b58ef8
Alexey Bakulin <abakulin@griddynamics.com>
2018-03-03 14:22:56 +0400

    Remove own rules engine

--- src/main/java/com/loyalty/demo/config/ApplicationConfig.java
+++ src/main/java/com/loyalty/demo/config/ApplicationConfig.java
@@ -1,54 +1,14 @@
 package com.loyalty.demo.config;
 
-import com.loyalty.demo.Rule;
-import com.loyalty.demo.RuleAction;
-import com.loyalty.demo.RulesRepository;
-import com.loyalty.demo.rule.condition.CartTotalCondition;
-import com.loyalty.demo.rule.condition.ProductNameCondition;
-import com.loyalty.demo.rule.effect.PercentDiscountForProductAction;
-import com.loyalty.demo.rule.effect.PercentDiscountForTotalAction;
-import com.loyalty.demo.service.RulesMatcher;
 import com.loyalty.demo.service.ShoppingCartProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.*;
-
 @Configuration
 public class ApplicationConfig {
-    @Bean
-    public RulesRepository discountsRepository() {
-        List<Rule> cache = new ArrayList<>();
-
-        Rule discount5percOnTv = new Rule(
-                new ProductNameCondition("ShoppingCart -> CartItem -> Product:name", "tv"),
-                new PercentDiscountForProductAction("ShoppingCart -> CartItem:price", "Discount 5%", 0.05, "tv")
-        );
-
-        Rule discount10percOnSmartphone = new Rule(
-                new ProductNameCondition("ShoppingCart->CartItem->Product:name", "smartphone"),
-                new PercentDiscountForProductAction("ShoppingCart -> CartItem:price", "Discount 10%", 0.1, "smartphone")
-        );
-
-        Rule discount5percOnTotalGreatThan500 = new Rule(
-                new CartTotalCondition("ShoppingCart:total", 500.0),
-                new PercentDiscountForTotalAction("ShoppingCart:total", "Discount 5%", 0.05)
-        );
-
-        cache.add(discount5percOnTv);
-        cache.add(discount10percOnSmartphone);
-        cache.add(discount5percOnTotalGreatThan500);
-
-        return new RulesRepository(cache);
-    }
 
     @Bean
-    public RulesMatcher rulesMatcher(RulesRepository repository) {
-        return new RulesMatcher(repository.findAll());
-    }
-
-    @Bean
-    public ShoppingCartProcessor shoppingCartProcessor(RulesMatcher rulesMatcher) {
-        return new ShoppingCartProcessor(rulesMatcher);
+    public ShoppingCartProcessor shoppingCartProcessor() {
+        return new ShoppingCartProcessor();
     }
 }
--- src/main/java/com/loyalty/demo/service/ShoppingCartProcessor.java
+++ src/main/java/com/loyalty/demo/service/ShoppingCartProcessor.java
@@ -6,32 +6,8 @@ import java.util.List;
 
 public class ShoppingCartProcessor {
 
-    private RulesMatcher rulesMatcher;
-
-    public ShoppingCartProcessor(RulesMatcher rulesMatcher) {
-        this.rulesMatcher = rulesMatcher;
-    }
-
-    public Rule processShoppingCart(ShoppingCart shoppingCart) {
-        List<Rule> matchRules = rulesMatcher.apply(shoppingCart);
-
-        // filter and return more profitable rule
-        return findMuchProfitableRule(matchRules, shoppingCart);
-    }
-
-    // brute force search by enumeration
-    private Rule findMuchProfitableRule(List<Rule> rules, ShoppingCart shoppingCart) {
-        Rule win = null;
-        for (Rule rule : rules) {
-
-            Double profit = rule.getEffect().calculate(shoppingCart);
-
-            if (win == null) win = rule;
-            else
-                win = (rule.getEffect().calculate(shoppingCart) > profit) ? rule : win;
-        }
-
-        return win;
+    public String processShoppingCart(ShoppingCart shoppingCart) {
+        return "Shopping cart has been processed";
     }
 
 }
