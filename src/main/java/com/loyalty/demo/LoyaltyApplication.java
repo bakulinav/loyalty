532e8ba23c02eb5722a7df710e9b312c89f79c7f
Alexey Bakulin <abakulin@griddynamics.com>
2018-03-03 14:45:11 +0400

    Intergation Drools to the Spring app

--- src/main/java/com/loyalty/demo/LoyaltyApplication.java
+++ src/main/java/com/loyalty/demo/LoyaltyApplication.java
@@ -1,14 +1,19 @@
 package com.loyalty.demo;
 
 import org.kie.api.runtime.KieContainer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
+//@EnableWebMvc
 public class LoyaltyApplication implements CommandLineRunner {
+
+	private static final Logger log = LoggerFactory.getLogger("LoyaltyApplication");
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoyaltyApplication.class, args);
@@ -16,12 +21,13 @@ public class LoyaltyApplication implements CommandLineRunner {
 
 	KieContainer kc;
 
+	@Autowired
 	public LoyaltyApplication(KieContainer kc) {
 		this.kc = kc;
 	}
 
 	@Override
 	public void run(String... strings) throws Exception {
-
+		log.info("Start Loyalty application");
 	}
 }
