60fe4ac550ee63c6432d0ee543f6df69a74387a8
Alexey Bakulin <abakulin@griddynamics.com>
2018-03-03 14:19:40 +0400

    Add Drools dependency

--- src/main/java/com/loyalty/demo/LoyaltyApplication.java
+++ src/main/java/com/loyalty/demo/LoyaltyApplication.java
@@ -1,14 +1,27 @@
 package com.loyalty.demo;
 
+import org.kie.api.runtime.KieContainer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
-public class LoyaltyApplication {
+public class LoyaltyApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoyaltyApplication.class, args);
+	}
+
+	KieContainer kc;
+
+	public LoyaltyApplication(KieContainer kc) {
+		this.kc = kc;
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+
 	}
 }
