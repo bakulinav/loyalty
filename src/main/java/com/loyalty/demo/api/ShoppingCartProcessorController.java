3802e7852220e7c2c3ae15563f07d12a5ed8f585
Alexey Bakulin <abakulin@griddynamics.com>
2018-03-03 14:35:45 +0400

    Turn off web support

--- src/main/java/com/loyalty/demo/api/ShoppingCartProcessorController.java
+++ src/main/java/com/loyalty/demo/api/ShoppingCartProcessorController.java
@@ -4,9 +4,9 @@ import com.loyalty.demo.ShoppingCart;
 import com.loyalty.demo.service.ShoppingCartProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ShoppingCartProcessorController {
@@ -18,9 +18,9 @@ public class ShoppingCartProcessorController {
         this.processor = processor;
     }
 
-    @PostMapping("/cart-processor")
-    @ResponseBody
-    public String processShoppingCart(@RequestBody ShoppingCart cart) {
-        return processor.processShoppingCart(cart);
-    }
+//    @PostMapping("/cart-processor")
+//    @ResponseBody
+//    public String processShoppingCart(@RequestBody ShoppingCart cart) {
+//        return processor.processShoppingCart(cart);
+//    }
 }
